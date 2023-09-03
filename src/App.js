cad2a35e523a04ab093a5de0cfcc78563b28af1d
Sukhmeet <sukhmeetsingh645@gmail.com>
2023-09-04 00:42:25 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+function App() {
+  return (
+    <>
+    <Navbar title="Txtmo" aboutText="About" />
+    <div className="container my-3">
+    <TextForm/>
+    </div>
+    </>
+  );
+}
+
+export default App;
