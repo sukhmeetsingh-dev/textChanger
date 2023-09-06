455dc4fbd62e8a542d36e3c2197c0af4e6c34c7c
Sukhmeet <sukhmeetsingh645@gmail.com>
2023-09-06 23:57:53 +0530

    improve dark mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Alert from './components/Alert';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
@@ -9,20 +10,31 @@ import React, { useState } from 'react'
 
 function App() {
   const [mode, setMode] = useState('light');
+  const [alert, setalert] = useState(null);
+
+  const showalert = (message, type) => {
+    setalert({
+      msg: message,
+      type: type
+    })
+  }
 
   const toggleMode = () => {
-    if(mode === 'light'){
+    if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
+      showalert("Dark mode has been enabled", "success");
     }
-    else{
+    else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showalert("Light mode has been enabled", "success");
     }
   }
   return (
     <>
       <Navbar title="Txtmo" aboutText="About" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
         <TextForm heading="Enter the text to analyze" mode={mode} />
         {/* <About/> */}
